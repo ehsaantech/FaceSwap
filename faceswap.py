afc4791b6f694c470afd395f3fb3dd56e6770086
ecspress <ecspress@hotmail.com>
2022-10-18 22:44:30 +0500

    fixed swapping;

--- faceswap.py
+++ faceswap.py
@@ -1,8 +1,7 @@
 import logging
 import cv2
-from face_swap import apply_mask, correct_colours, mask_from_points, warp_image_3d
+from face_swap import mask_from_points, warp_image_3d
 import numpy
-from compare import calculateDelaunayTriangles, warpTriangle
 from utilities.cli import takeCliOptionSelection
 from utilities.image import readImage, writeImage
 from utilities.videoprocessor import VideoProcessor
@@ -92,100 +91,17 @@ def _faceSwap(srcImage, srcFaceFeatures, targetImage, targetFacesWithFeatures):
   return outputImage
 
 def face_swap_custom(srcImage, targetImage, srcFaceFeatures, targetFaceFeatures):
-  # h, w = targetImage.shape[:2]
-
-  # srcHull = srcFaceFeatures
-  # targetHull = targetFaceFeatures
-  # # srcHull = []
-  # # targetHull = []
-  # # hullIndex = cv2.convexHull(targetFaceFeatures, returnPoints = False)
-  # # for i in range(0, len(hullIndex)):
-  # #   srcHull.append(srcFaceFeatures[int(hullIndex[i])])
-  # #   targetHull.append(targetFaceFeatures[int(hullIndex[i])])
-
-  # # 3d warp
-  # warped_src_face = warp_image_3d(srcImage, srcHull, targetHull, (h, w))
-  
-  # # Mask for blending
-  # mask = mask_from_points((h, w), targetHull, erode_flag=False)
-  # mask_src = numpy.mean(warped_src_face, axis=2) > 0
-  # mask = numpy.asarray(mask * mask_src, dtype=numpy.uint8)
-
-  # # Correct color
-  # warped_src_face = apply_mask(warped_src_face, mask)
-  # dst_face_masked = apply_mask(targetImage, mask)
-  # warped_src_face = correct_colours(dst_face_masked, warped_src_face, targetHull)
-  
-  # #Poisson Blending
-  # r = cv2.boundingRect(mask)
-  # center = ((r[0] + int(r[2] / 2), r[1] + int(r[3] / 2)))
-  # output = cv2.seamlessClone(warped_src_face, targetImage, mask, center, cv2.NORMAL_CLONE)
-  # return output
-
-
-  warpedImage = targetImage.copy()
-
-  srcHull = []
-  targetHull = []
-  hullIndex = cv2.convexHull(targetFaceFeatures, returnPoints = False)
-  for i in range(0, len(hullIndex)):
-    srcHull.append(srcFaceFeatures[int(hullIndex[i])])
-    targetHull.append(targetFaceFeatures[int(hullIndex[i])])
-
-  # Find delanauy traingulation for convex hull points
   h, w = targetImage.shape[:2]
-  rect = (0, 0, w, h)
-     
-  dt = calculateDelaunayTriangles(rect, targetHull)
-    
-  if len(dt) == 0:
-    return
-
-  # Apply affine transformation to Delaunay triangles
-  for i in range(0, len(dt)):
-    t1 = []
-    t2 = []
-    
-    #get points for img1, img2 corresponding to the triangles
-    for j in range(0, 3):
-      t1.append(srcHull[dt[i][j]])
-      t2.append(targetHull[dt[i][j]])
-    
-    warpTriangle(srcImage, warpedImage, t1, t2)
+  warpedImage = warp_image_3d(srcImage, srcFaceFeatures, targetFaceFeatures, (h, w))
 
   # Mask for blending
-  mask = mask_from_points((h, w), targetFaceFeatures)
+  mask = mask_from_points((h, w), targetFaceFeatures, erode_flag=False)
   mask_src = numpy.mean(warpedImage, axis=2) > 0
   mask = numpy.asarray(mask * mask_src, dtype=numpy.uint8)
 
-  # Correct color
-  warpedImage = apply_mask(warpedImage, mask)
-  dst_face_masked = apply_mask(targetImage, mask)
-  warpedImage = correct_colours(dst_face_masked, warpedImage, targetFaceFeatures)
-  
-  # Poisson Blending
+  #Poisson Blending
   r = cv2.boundingRect(mask)
   center = ((r[0] + int(r[2] / 2), r[1] + int(r[3] / 2)))
   output = cv2.seamlessClone(warpedImage, targetImage, mask, center, cv2.NORMAL_CLONE)
   return output
-
-  # # Calculate Mask
-  # hull8U = []
-  # for i in range(0, len(targetHull)):
-  #     hull8U.append((targetHull[i][0], targetHull[i][1]))
-  
-  # mask = numpy.zeros(targetImage.shape, dtype = targetImage.dtype)  
-  # writeImage(mask, "output/t2.jpg")
-  
-  # cv2.fillConvexPoly(mask, numpy.int32(hull8U), (255, 255, 255))
-  # print('a')
-  # r = cv2.boundingRect(numpy.float32([targetHull]))    
-  # print('b')
-  # center = ((r[0]+int(r[2]/2), r[1]+int(r[3]/2)))
-  # print('c', center)
-      
-  # # # Clone seamlessly.
-  # output = cv2.seamlessClone(numpy.uint8(warpedImage), targetImage, mask, center, cv2.NORMAL_CLONE)
-  # writeImage(output, "output/t3.jpg")
-  # return output
     
